3a1d1daeb17f1c4faba8fdcec0021ae2f4decea3
Flavio Ever <feverton@uolinc.com>
2025-10-10 11:50:55 -0300

    feat: configurar CORS com métodos permitidos no bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
 
   await app.listen(process.env.PORT ?? 3001);
 }
 
-bootstrap();
+void bootstrap();
